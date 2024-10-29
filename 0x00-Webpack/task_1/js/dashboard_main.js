944e190dddff5eddc2f9174bca6158a7159b0063
dina <dinahamadi.20@gmail.com>
2024-10-29 01:19:20 +0100

    edit task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     let count = 0;
     function updateCounter() {
         count += 1;
-        $('#count').html('${count} clicks on the button');
+        $('#count').html(`${count} clicks on the button`);
     };
     $('button').on('click', _.debounce(updateCounter, 500));
 });
