aadeb4ec41a401c16b2709e4099250a3277c4ee7
dina <dinahamadi.20@gmail.com>
2024-10-29 03:29:16 +0100

    edit 2

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,6 +2,8 @@ import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
 $(document).ready(function () {
+    const logoDiv = $('<div id="logo"></div>');
+    $('body').append(logoDiv);
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<button>Click here to get started</button>');
