f6d5d5a4ba400be3a186532948c1c15af8eaf959
dina <dinahamadi.20@gmail.com>
2024-12-10 02:29:14 +0100

    task 9

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,51 +1,49 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/',
     },
     module: {
         rules: [
             {
-                test: /\.css$/i,
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
                 use: [
                     {
                         loader: 'file-loader',
                         options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images/',
-                        },
-                    },
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            bypassOnDebug: true,
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'assets/',
+                            publicPath: 'assets/',
                         },
                     },
                 ],
             },
         ],
     },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './dist/index.html',
-        }),
-    ],
-
     resolve: {
-        extensions: ['.js', '.json'],
+        extensions: ['.js', '.jsx'],
     },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../dist'),
+        },
+        hot: true,
+    },
+    devtool: 'inline-source-map',
+    mode: 'development',
 };
---
+++ 0x01-react_intro/task_5/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+export function getFullYear() {
+    return new Date().getFullYear();
+}
+export function getFooterCopy(isIndex) {
+    if (isIndex === true)
+    return "ALX";
+    else return "ALX main dashboard";
+}
+export function getLatestNotification() {
+    return "<strong>Urgent requirement</strong> - complete by EOD";
+}
